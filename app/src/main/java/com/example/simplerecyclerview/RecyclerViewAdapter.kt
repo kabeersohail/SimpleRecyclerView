6c4e667081e3837b5846da9462db1164a58311bd
SC46547 <mohammad.sohail@external.stellantis.com>
2021-11-30 13:43:40 +0530

    made code refactor

--- app/src/main/java/com/example/simplerecyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/simplerecyclerview/RecyclerViewAdapter.kt
@@ -1,30 +1,49 @@
 package com.example.simplerecyclerview
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerViewAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
 
     var data = listOf<ModelX>()
-
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.text
+        holder.bind(item)
     }
 
     override fun getItemCount() = data.size
+
+    class MyViewHolder private constructor(item: View) : RecyclerView.ViewHolder(item) {
+        private val titleText: TextView = item.findViewById(R.id.my_title)
+        private val image: ImageView = item.findViewById(R.id.my_image_view)
+
+        fun bind(item: ModelX){
+            titleText.text = item.text
+            image.setImageResource(R.drawable.info)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.single_item, parent, false)
+                return MyViewHolder(view)
+            }
+        }
+
+    }
+
 }
